d4ebc5af462b5ba967be0fdb42578d83627919fc
davydov <4peopleevgeny@gmail.com>
2020-04-05 22:22:53 +0300

    inited app

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <nuxt />
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import {Component} from 'vue-property-decorator'
+
+  @Component({
+    name: 'default-page',
+  })
+  export default class DefaultPage extends Vue {
+    get meta () {
+      return [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, maximum-scale=1 shrink-to-fit=no'
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Главная'
+        }
+      ]
+    }
+
+    protected head () {
+      const canonical = `https://mysite.com${this.$route.path
+        .toLowerCase()
+        .replace(/\/$/, '')}`
+      return {
+        meta: [
+          ...this.meta
+
+        ],
+        script: [
+          // { src: 'https://markknol.github.io/console-log-viewer/console-log-viewer.js' }
+        ],
+        link: [{ rel: 'canonical', href: canonical }]
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Привет Хабр!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import {Component} from 'vue-property-decorator'
+
+  @Component({
+    name: 'root'
+  })
+  export default class Root extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
